dad45858a9e998ad42a51448689f7ec4130900c1
yairshinar <40474391+yairshinar@users.noreply.github.com>
2019-11-19 12:24:28 +0200

    Update Two_sum.cpp

--- Two_sum.cpp
+++ Two_sum.cpp
@@ -1,7 +1,7 @@
 //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
+//On**2
 class Solution {
 public:
      vector<int> twoSum(vector<int>& nums, int target) {
@@ -16,3 +16,39 @@ public:
         return vect;
     }
 };
+
+//On
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+class Solution {
+public:
+
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> vect;
+        map<int, int> umap;
+        map<int, int>::iterator it = umap.begin();
+
+        for (int init = 0; init < nums.size(); init++) {
+            umap.insert(pair<int, int>(nums[init], init));
+        }
+        for (int i = 0; i < nums.size(); i++) {
+
+
+            it = umap.find(target - nums[i]);
+            if (it != umap.end() && umap[target - nums[i]] != i) {
+
+                vect.push_back(i);
+                vect.push_back(umap[target - nums[i]]);
+
+                return vect;
+
+            }
+        }
+        return vect;
+    }
+}; 
